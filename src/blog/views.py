3bbbd62cf62b951a2fd5970a2211afa88cdea527
Hari Mahat <hari.mahat@valamis.com>
2022-07-28 16:34:02 +0300

    Added create post view

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from .models import Comment, Post
+from .forms import PostForm, CommentForm
 
 
 # Create your views here.
@@ -12,6 +14,28 @@ from .models import Comment, Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by(
         "-created_on")  # latest post on top
+
+
+class CreatePost(LoginRequiredMixin, generic.CreateView):
+    login_url = reverse_lazy("login")  # navigate to login page
+    # after success navigate to post_list page
+    success_url = reverse_lazy("post_list")
+
+    # create form
+    form_class = PostForm
+    queryset = Post.objects.all()
+    template_name = "blog/post_form.html"
+
+    def post(self, request, *args, **kwargs):
+        request.POST._mutable = True
+        request.POST['author'] = request.user
+        request.POST._mutable = False
+
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
 class CreateUser(generic.CreateView):
