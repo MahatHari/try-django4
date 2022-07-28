08c3aaa81da7f6139fe9d178bc555367ede47b50
Hari Mahat <hari.mahat@valamis.com>
2022-07-28 11:28:01 +0300

    added git ignore

--- src/src/urls.py
+++ src/src/urls.py
@@ -28,5 +28,5 @@ urlpatterns = (
         path('accounts/logout', views.LogoutView.as_view(), name='logout')
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
