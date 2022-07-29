3e45a98978531ba412aa710ebf6ea185bc49e3b2
Hari Mahat <hari.mahat@valamis.com>
2022-07-29 13:16:12 +0300

    added comment to post, error in comment approval,

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Comment, Post
 from .forms import PostForm, CommentForm
 
 
 # Create your views here.
-
+# class based views
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by(
@@ -42,6 +44,68 @@ class CreatePost(LoginRequiredMixin, generic.CreateView):
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
+    login_url = reverse_lazy('login')
+
+    queryset = Post.objects.all()
+    form_class = PostForm
+    # uses same template as CreatePost, post_form.html
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', args=({self.object.id, }))
+
+
+class PostDraftList(LoginRequiredMixin, generic.ListView):
+    login_url = reverse_lazy('login')
+    queryset = Post.objects.filter(status=0).order_by('-created_on')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(author=self.request.user)
+
+
+class PostArchivedList(LoginRequiredMixin, generic.ListView):
+    login_url = reverse_lazy('login')
+    queryset = Post.objects.filter(status=2).order_by('-created_on')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(author=self.request.user)
+
+# function based view
+
+
+@login_required
+def add_comment(request, pk):
+    post = Post.objects.filter(pk=pk).first()
+    if request.method == "POST":
+        request.POST._mutable = True
+        request.POST["author"] = request.user
+        request.POST["post"] = post
+        request.POST._mutable = False
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/comment_form.html", {"form": form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = Comment.objects.filter(pk=pk)
+    post_pk = comment.first().post.pk
+    comment.update(approved_comment=True)
+    return redirect("post_detail", post_pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = Comment.objects.filter(pk=pk)
+    post_pk = comment.first().post.pk
+    comment.delete()
+    return redirect('post_detail', post_pk)
 
 
 class CreateUser(generic.CreateView):
